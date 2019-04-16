62837a7f7092df10ac1d9ff1a241839359757661
Devin Leaman <dleaman212@gmail.com>
2019-04-16 12:09:09 -0500

    Added some more tests and outlines for future tests

--- test/AgendaidUtil.test.js
+++ test/AgendaidUtil.test.js
@@ -9,6 +9,42 @@ test('getCollectionName() should get a valid collection name based on the (optio
   expect(util.getCollectionName(utcMs)).toBe(`${futrDate.getFullYear()}.${getWeekNumber(futrDate)}`)
 })
 
+test('getTriggerTime() should get the correct UTC ms for the given time offset', () => {
+  /** The amount of milliseconds equal to 3 days, 2 hours, and 15 minutes */
+  const MS_OFFSET = 267300000
+  const triggerTime = util.getTriggerTime({ days: 3, hours: 2, minutes: 15 })
+  const testTime = new Date().getTime() + MS_OFFSET
+  expect(triggerTime).toBe(testTime)
+})
+
+test('parseArgs() should parse the given String and return the relevant data', () => {
+  const testStr = 'Go to the store for milk & eggs in 3 days 2 hours and 15 minutes'
+  const testArr = testStr.split(' ')
+
+  expect(util.parseArgs(testArr)).toEqual({
+    reminderMsg: 'Go to the store for milk & eggs',
+    days: 3,
+    hours: 2,
+    minutes: 15
+  })
+})
+
+test('parseUserInput() should return an object of clean data for a reminder', () => {
+  const testData = {}
+})
+
+test('getTimeVal() should get the amount of time from the given parameters', () => {
+
+})
+
+test('getReminderMsg() should get the content of the reminder from the message sent by the user', () => {
+
+})
+
+test('getTimeArgs() should get the portion of the user provided args that only contains the time data for the reminder', () => {
+
+})
+
 /**
  * Retrieves the ISO week number of the provided date using native Javascript
  * functions.
